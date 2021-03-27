ed23db66dabeb4e7dc33a428be303417573a9b5b
MsNahid <m.nahidzaman@gmail.com>
2021-03-27 23:12:41 +0600

    Added media folder settings on settings

--- searchPro/settings.py
+++ searchPro/settings.py
@@ -126,4 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'searchpro/static')
-] 
+]
+
+# Media Folder Setting
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
