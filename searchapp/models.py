a5bffaec6d259d42c22fe37b322dd4f9b096359e
MsNahid <m.nahidzaman@gmail.com>
2021-03-27 22:08:03 +0600

    Created model named 'musician' with fields

--- searchapp/models.py
+++ searchapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Musician(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.FloatField()
+    city = models.CharField(max_length=50)
+    user_photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    yearly_income = models.IntegerField()
+    description = models.TextField()
+    list_date = models.DateTimeField(default=datetime.now())
+
+    def __str__(self):
+        return self.name
+    
