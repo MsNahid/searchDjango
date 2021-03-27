a5d2df6f12f834ca82633e50a23104b3bd9ec266
MsNahid <m.nahidzaman@gmail.com>
2021-03-28 01:48:03 +0600

    Display musicians in templates

--- searchapp/views.py
+++ searchapp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Musician
+
 # Create your views here.
 def home(request):
-    return render(request, 'searchapp/home.html')
+    musicians = Musician.objects.all()
+
+    context = {
+        'musicians': musicians
+
+    }
+    return render(request, 'searchapp/home.html', context)
 
 
 
